cbef8fce1a610d99bd32f053188ae5f0b38cc85e
KongPleiku <154966174+KongPleiku@users.noreply.github.com>
2025-09-02 17:42:58 +0700

    add temparory trainer script

---
+++ functional/trainer_bitandbytes.py
@@ -0,0 +1,8 @@
+import torch 
+from torch import functional as F
+from bitsandbytes import functional as bitF
+
+#this file is using the bitandbytes 8bit optimizer to train model
+
+def train():
+    return
