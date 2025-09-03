999b55b047b71e01c30ac1df543ee0be54b43bbf
KongPleiku <154966174+KongPleiku@users.noreply.github.com>
2025-09-03 23:08:51 +0700

    make way for embedder module and add simple txt file to understand the core of the Diffusion model

---
+++ module/baseModule.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import math
+from timm.models.vision_transformer import PatchEmbed, Attention, Mlp
+
+
+
+#################################
+#           EMBEDDER            #    
+#################################
+
+class TimestepEmbedder(nn.modules):
+    def __init__():
+        pass
+
+    def forward():
+        pass
+
+class LabelsEmbedder(nn.modules):
+    def __init__():
+        pass
+
+    def forward():
+        pass
